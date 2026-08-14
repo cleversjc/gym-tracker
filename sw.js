ccb255a1c1fd3d0d9d808624671b6c597237c13c
cleversjc <cxsx2000918@gmail.com>
2026-08-14 13:34:12 +0800

    Refresh PWA cache for V1.4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="gym-tracker-v1.3";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE="gym-tracker-v1.4";
+const ASSETS=["./","./index.html","./styles.css","./enhancements.css","./app.js","./enhancements.js","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
