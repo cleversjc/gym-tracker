16b9e7686bba1982f7fd97c1eeaedbbcf0c6db31
cleversjc <cxsx2000918@gmail.com>
2026-08-18 13:22:40 +0800

    Bump PWA cache to V1.6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="gym-tracker-v1.5";
-const ASSETS=["./","./index.html","./styles.css","./enhancements.css","./app.js","./enhancements.js","./rest-timer.js","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE="gym-tracker-v1.6";
+const ASSETS=["./","./index.html","./styles.css","./enhancements.css","./ui-v16.css","./app.js","./enhancements.js","./rest-timer.js","./ui-v16.js","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
