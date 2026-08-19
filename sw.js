e7d21c524e2544a55a06347192e79ddaac827c71
cleversjc <cxsx2000918@gmail.com>
2026-08-19 14:20:02 +0800

    Bump PWA cache after custom exercise modal fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="gym-tracker-v1.7";
+const CACHE="gym-tracker-v1.7.1";
 const ASSETS=["./","./index.html","./styles.css","./enhancements.css","./ui-v16.css","./custom-exercises.css","./app.js","./enhancements.js","./rest-timer.js","./ui-v16.js","./custom-exercises.js","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
